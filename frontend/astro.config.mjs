2cb07623a477521641617abff6edc33cff70d933
Yutaka Kizaki <factory0611@gmail.com>
2026-04-12 21:08:50 +0900

    SSG修正

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,5 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
-  site: 'https://iwakiki.jp',
-  output: 'server', // 🔥これ
+  site: 'https://iwakiki.co.jp',
 
   integrations: [
     react(),
